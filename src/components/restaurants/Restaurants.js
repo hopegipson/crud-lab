6315f3cf24257cbebeb890b672b9e59c28808bf4
hopegipson <gipson.hope@gmail.com>
2021-03-24 19:51:54 -0500

    Done.

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Restaurant from '../restaurants/Restaurant'
 
 class Restaurants extends Component {
+
+  renderRestaurants = () => this.props.restaurants.map((restaurant) => <Restaurant key={restaurant.id} delete={this.props.delete} restaurant ={restaurant} />) 
+
   render() {
     return(
       <ul>
-        Restaurants Component
+        {this.renderRestaurants()}
       </ul>
     );
   }
--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -2,11 +2,45 @@ import React, { Component } from 'react';
 import Reviews from './Reviews';
 
 class ReviewInput extends Component {
+
+  state = {
+    text: '',
+  restaurantId:  this.props.restaurantId }
+
+  handleOnChange(event) {
+    this.setState({
+      text: event.target.value,
+    });
+  }
+
+  handleOnSubmit(event) {
+    event.preventDefault();
+   this.props.addReview(this.state);
+    this.setState({
+      text: '',
+    });
+  }
+
+  filterReviews = () => {
+    if (this.props.reviews){
+    return this.props.reviews.filter(review => review.restaurantId === this.props.restaurantId)
+    }
+  }
+  
+
   render() {
     return (
       <div>
-        Review Input
-      </div>
+
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+        <input
+          type="text"
+          value={this.state.text}
+          onChange={(event) => this.handleOnChange(event)} />
+        <input type="submit" />
+      </form>
+      <Reviews reviews={this.filterReviews()} deleteReview={this.props.deleteReview}/>
+    </div>
     );
   }
 };
